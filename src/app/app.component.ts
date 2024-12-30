ae61e4eccb8568aa43a297c2f23661d453123783
Aswin060104 <trajaaswin@gmail.com>
2024-12-30 23:43:32 +0530

    bar chart is implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-visualization-demo';
+  lineChartData = [
+    { 
+      data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' 
+    }
+  ];
+  lineChartLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  lineChartOptions = {
+    responsive: true
+  };
+  lineChartColors = [
+    {
+      borderColor: 'rgba(0,99,132,1)',
+      backgroundColor: 'rgba(0,0,0,0.2)'
+    }
+  ];
+  lineChartLegend = true;
+  lineChartType = 'line';
+
+  radarChartData = [
+    { data: [65, 59, 90, 81, 56, 55], label: 'Series A' }
+  ];
+  radarChartLabels = ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling'];
+  radarChartOptions = {
+    responsive: true
+  };
+  radarChartColors = [
+    {
+      backgroundColor: 'rgba(255,99,132,0.2)',
+      borderColor: 'rgba(255,99,132,1)',
+      pointBackgroundColor: 'rgba(255,99,132,1)'
+    }
+  ];
+  radarChartLegend = true;
+  radarChartType = 'radar';
+
 }
